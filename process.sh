943db27cf51e41f6305758790506ca1c4a0ee879
Jason D Limberis <8179171+SemiQuant@users.noreply.github.com>
2021-10-19 10:51:52 -0700

    automating

---
+++ process.sh
@@ -0,0 +1,15 @@
+sample="$1"
+ref="$2"
+R1="${sample}_R1_001.fastq.gz"
+R2="${sample}_R2_001.fastq.gz"
+
+
+bwa mem "$ref" "$R1" "$R2" > "${sample}.sam"
+samtools view -bS "${sample}.sam" | samtools sort - > "${sample}.bam"
+samtools index "${sample}.bam"
+samtools flagstat "${sample}.bam" >  "${sample}.flagstat.txt"
+samtools depth -a "${sample}.bam" -o "${sample}.depth.tsv"
+
+samtools view -F 4 "${sample}.bam" | gawk '{ if ( and($2, 16) == 0 ) { strand="+" } else { strand="-" }; print $3 "\t" $4 "\t" $4 + length($10) "\t"  strand }' > "${sample}.counts.tsv"
+
+Rscript "stillPCR_plots.R" "${pwd}/${sample}.counts.tsv"
---
+++ stillPCR_plots.R
@@ -0,0 +1,56 @@
+# Need to make it so it collapses reads that are close
+
+args = commandArgs(trailingOnly=TRUE)
+f_in <- args[1]
+
+name <- gsub(".counts.tsv", "", basename(f_in))
+
+
+filter_thresh <- 1000
+require(tidyverse)
+require(ggplot2)
+require(cowplot)
+dat <- read_tsv(f_in, col_names = F)
+
+
+dat$count <- 1
+
+dat <- dat %>% 
+  # select(-X1) %>% 
+  group_by_all() %>% 
+  summarise(count = sum(count)) %>% 
+  filter(count > filter_thresh) %>% 
+  ungroup()
+
+
+colnames(dat) <- c("Gene", "Start", "End", "Strand", "Count")
+
+write.table(file = paste0(name, "summarized_counts.tsv"), x = dat, sep = "\t", col.names = T, row.names = F)
+
+
+
+round_any = function(x, accuracy, f=round){f(x/ accuracy) * accuracy}
+dat$Start <- round_any(dat$Start, 10, f = floor)
+dat$End <- round_any(dat$End, 10, f = floor)
+dat <- dat %>% 
+  group_by(Gene, Start, End, Strand) %>% 
+  summarise(Count = sum(Count)) %>% 
+  filter(Count >= filter_thresh)
+
+
+plots <- list()
+genes <- unique(dat$Gene)
+for (i in 1:length(genes)){
+  tmp <- dat %>% 
+    filter(Gene == genes[i])
+  plots[[i]] <- ggplot(tmp, aes(x = Start, xend = End, y = Count, yend = Count, color = Strand)) + 
+    xlab("Position") + ylab("Depth") +
+    geom_segment() + expand_limits(y=0)
+}
+
+# p <- ggplot(dat, aes(x = Start, xend = End, y = Count, yend = Count, color = Strand)) + 
+#   geom_segment() + expand_limits(y=0)
+
+pdf(paste0(name, ".pdf"))
+ggpubr::ggarrange(plotlist = plots, labels = genes, common.legend = T, legend = "bottom")
+dev.off()
